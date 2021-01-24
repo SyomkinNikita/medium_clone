8b47564041a1bd7190d70e445375baf6149abc14
nikita <79851214062@yandex.ru>
2021-01-25 00:28:15 +0300

    init

---
+++ src/pages/routes.js
@@ -0,0 +1,19 @@
+import {Switch, Route} from 'react-router-dom'
+import GlobalFeed from "./globalFeed";
+import Article from "./article";
+import Authentication from "./authentication";
+import TagFeed from "./tagFeed";
+import YourFeed from "./yourFeed";
+
+export default () => {
+    return (
+        <Switch>
+            <Route path='/' component={GlobalFeed} exact/>
+            <Route path='/feed' component={YourFeed}/>
+            <Route path='/tags/:slug' component={TagFeed}/>
+            <Route path='/login' component={Authentication}/>
+            <Route path='/register' component={Authentication}/>
+            <Route path='/articles/:slug' component={Article}/>
+        </Switch>
+    )
+}
